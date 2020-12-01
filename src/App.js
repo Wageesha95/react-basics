6d7e9e368abe21a26fe79da661b8c269c8b41bcb
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2020-12-01 17:39:45 +0530

    3.11 Thinking in JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello All'
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <p>{this.state.string}</p>
+        <button onClick={() => this.setState({ string: 'Hello Thilina,' })}>Change text</button>
+      </div>
+
+
+    )
+  }
 }
 
 export default App;
+
