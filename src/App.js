0e316eed917c8f277f77468e7b8527bad5f3dec0
Thilina Liyanage <37930455+Wageesha95@users.noreply.github.com>
2020-12-03 18:02:52 +0530

    3.29 class methods and event binding

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends Component {
       .then(users => { this.setState({ monsters: users }) })
   }
 
+  handleChange = (e) => {
+    e => this.setState({ searchField: e.target.value })
+  }
+
   render() {
 
     const { monsters, searchField } = this.state;
@@ -29,7 +33,7 @@ class App extends Component {
       <div className='App'>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}> </CardList>
       </div>
